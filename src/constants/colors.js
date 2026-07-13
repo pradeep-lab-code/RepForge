32a29aa1b859b7266fd702e99333dc3f76a5d92c
Pradeepsingh <hlopradeep7@gmail.com>
2026-07-13 15:28:02 +0530

    Initial project setup

---
+++ src/constants/colors.js
@@ -0,0 +1,15 @@
+export const COLORS = {
+  primary: "#4F46E5",
+  secondary: "#22C55E",
+  ai: "#8B5CF6",
+  warning: "#F59E0B",
+  danger: "#EF4444",
+
+  background: "#0F172A",
+  surface: "#1E293B",
+
+  white: "#FFFFFF",
+  secondaryText: "#94A3B8",
+
+  border: "#334155",
+};
